cf8d1704b4130083055bc94c7d39851791c3762f
sunpark20113281 <reverse199@gmail.com>
2017-01-02 20:08:21 +0900

    Prob2920

---
+++ BOJ/BOJ/Prob2920.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int a[8];
+
+int main() {
+	for (int i = 0; i < 8; i++)
+		scanf("%d", &a[i]);
+	
+	int tmp = a[0];
+	if (a[0] == 1) {
+		for (int i = 1; i < 8; i++) {
+			if (a[i] != ++tmp) {
+				printf("mixed\n");
+				return 0;
+			}
+		}
+		printf("ascending\n");
+		return 0;
+	}
+	else if (a[0] == 8) {
+		for (int i = 1; i < 8; i++) {
+			if (a[i] != --tmp) {
+				printf("mixed\n");
+				return 0;
+			}
+		}
+		printf("descending\n");
+		return 0;
+	}
+	else
+		printf("mixed\n");
+
+}
